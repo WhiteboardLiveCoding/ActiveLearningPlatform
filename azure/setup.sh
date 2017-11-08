dbe0504160f4db70b72ebf256d6799e8443360fe
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2017-11-08 01:19:01 +0000

    Install dependencies, if missing

--- azure/setup.sh
+++ azure/setup.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
+install_git()
+{
+	echo "Installing git..."
+	sudo apt update
+	sudo apt -y install git || exit 1
+}
+
+install_python3()
+{
+	echo "Installing python3..."
+	sudo apt update
+	sudo apt -y install python3 || exit 1
+}
+
+install_pip3()
+{
+	echo "Installing pip3..."
+	sudo apt update
+	sudo apt -y install python3-pip || exit 1
+}
+
 echo "Bootstraping Active Learning Platform..."
 
 echo "Checking requirements..."
-command -v git >/dev/null 2>&1 || { echo >&2 "Missing Git. Aborting."; exit 1; }
-command -v python3 >/dev/null 2>&1 || { echo >&2 "Missing Python. Aborting."; exit 1; }
-command -v pip3 >/dev/null 2>&1 || { echo >&2 "Missing Pip. Aborting."; exit 1; }
+command -v git >/dev/null 2>&1 || { echo >&2 "Missing git."; install_git; }
+command -v python3 >/dev/null 2>&1 || { echo >&2 "Missing python."; install_python3; }
+command -v pip3 >/dev/null 2>&1 || { echo >&2 "Missing pip."; install_pip3; }
 
 echo "Cloning WhiteboardLiveCoding/ActiveLearningPlatform..."
 git clone https://github.com/WhiteboardLiveCoding/ActiveLearningPlatform.git alp
