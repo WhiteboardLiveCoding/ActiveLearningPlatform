1b9d3dcca9938fb75800ffa0fc90ccf854dfaa87
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2017-11-13 18:04:31 +0000

    List pip3 installs

--- azure/setup.sh
+++ azure/setup.sh
@@ -96,6 +96,7 @@ git checkout convert-images-to-dataset
 
 echo "Installing project requirements..."
 pip3 install -r requirements.txt
+pip3 list
 
 echo "Running [alp.py]..."
 python3 alp.py -i pictures -a code
